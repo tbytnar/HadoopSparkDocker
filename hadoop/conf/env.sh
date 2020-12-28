0b2dd36f5f0b78ef8e33796325d5de48f42db5bb
Tim Bytnar <tbytnar@gmail.com>
2020-12-28 09:37:59 -0600

    initial commit

---
+++ hadoop/conf/env.sh
@@ -0,0 +1,5 @@
+echo "
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop/
+" >> /usr/local/hadoop/etc/hadoop/hadoop-env.sh
